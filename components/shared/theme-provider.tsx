107256b95f2e719109757911255c8c2d66aec48e
Maxime Ribes <maximeribes33@gmail.com>
2026-09-18 23:20:43 +0200

    fix(theme): eliminate React 19 script warning and purge Turbopack module cache

--- components/shared/theme-provider.tsx
+++ components/shared/theme-provider.tsx
@@ -4,5 +4,16 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Évite le rendu des scripts inline côté client avant hydratation sous React 19
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
